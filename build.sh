af79df9214d0fba2225c1440fcf35ef6d3365e71
John Warburton <john@johnwarburton.com>
2014-10-07 21:58:30 +0100

    Compile for any CPU, not just my old machine

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@
 # My aim is to make the cross compilation script default to these values
 # so that this command line can be discarded.
 
-./cross_compile_ffmpeg_cygwin64.sh --build-ffmpeg-shared=n --build-ffmpeg-static=y --disable-nonfree=n --sandbox-ok=y --build-libmxf=y --build-mp4box=y --build-choice=win64 --cflags=-march=core2 --git-get-latest=y --prefer-stable=n
+./cross_compile_ffmpeg_cygwin64.sh --build-ffmpeg-shared=n --build-ffmpeg-static=y --disable-nonfree=n --sandbox-ok=y --build-libmxf=y --build-mp4box=y --build-choice=win64 --git-get-latest=y --prefer-stable=n
 
