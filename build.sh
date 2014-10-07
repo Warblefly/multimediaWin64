03f21e8b93da6aa3b7796d6cb7a0465209bb34b5
John Warburton <john@johnwarburton.com>
2014-10-07 11:41:44 +0100

    Revert "Launch cross-compile with correct parameters"
    This reverts commit b81245e6cff97942d3046d5b2e04353b8428ade7.

--- build.sh
+++
@@ -1,9 +0,0 @@
-#|/bin/bash
-
-# This does nothing but call the script with the paramenters I use on a daily basis.
-# No other parameters have been tested.
-# My aim is to make the cross compilation script defualt to these values, then
-# this command line can be discarded.
-
-./cross_compile_ffmpeg_cygwin64.sh --build-ffmpeg-shared=n --build-ffmpeg-static=y --disable-nonfree=n --sandbox-ok=y --build-libmxf=y --build-mp4box=y --build-choice=win64 --cflags=-march=core2 --git-get-latest=y --prefer-stable=n
-
