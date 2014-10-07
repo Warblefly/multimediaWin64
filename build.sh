4891d8aceae1f2ce2a07de8223c115b3906e5ceb
John Warburton <john@johnwarburton.com>
2014-10-07 11:48:22 +0100

    Launch cross-compile with correct parameters

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This calls the script with the parameters I use daily.
+# No other parameters have been tested.
+# My aim is to make the cross compilation script default to these values
+# so that this command line can be discarded.
+
+./cross_compile_ffmpeg_cygwin64.sh --build-ffmpeg-shared=n --build-ffmpeg-static=y --disable-nonfree=n --sandbox-ok=y --build-libmxf=y --build-mp4box=y --build-choice=win64 --cflags=-march=core2 --git-get-latest=y --prefer-stable=n
+
